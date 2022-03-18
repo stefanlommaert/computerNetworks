e6658f494e70cfb4aa2f620895dfd8e168375acd
Stefan <stefan.lommaert@student.kuleuven.be>
2022-03-18 15:59:06 +0100

    scrape page

--- client.py
+++ client.py
@@ -2,10 +2,7 @@ from cgi import test
 import re
 import socket
 
-# client.connect(ADDR)
-
-
-ip = socket.gethostbyname('www.example.com')
+ip = socket.gethostbyname('www.tcpipguide.com')
 SERVER = ip
 PORT = 80
 ADDR = (SERVER, PORT)
@@ -20,11 +17,20 @@ def send(msg):
 
 msg = "GET / HTTP/1.1\r\nHost:%s\r\n\r\n" % SERVER
 send(msg)
-response = client.recv(1000).decode(FORMAT)
-# print(response)
+
+response = client.recv(30000).decode(FORMAT)
+print(response)
 for i in range(len(response)):
     if (response[i:i+14]) == "Content-Length":
-        contentLength = int(response[i+16:i+19]) #TODO: slice tot enter begint, niet tot character i+19
-        print(response[-(contentLength):])
+        contentLength = int(response[i+16:i+22]) #TODO: slice tot enter begint, niet tot character i+19
+        # print(contentLength)
+        body = (response[-(contentLength):])
 
+f = open("htmlBody.html", "w")
+f.write(body)
+f.close()
+
+#open and read the file after the appending:
+# f = open("htmlBody.html", "r")
+# print(f.read()) 
 
